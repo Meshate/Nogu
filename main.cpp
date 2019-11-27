4d6bf2e3fcb069fd335f382bad9e2e227d68b80d
meshate <li19638176@gmail.com>
2019-11-28 01:10:03 +0800

    #2

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <atomic>
+#include <chrono>
+#include <ctime>
+#include "threadpool.h"
+
+using namespace std;
+
+static atomic<int> num = 0;
+
+
+
+int main() {
+    nogu::threadpool pool(4);
+//    vector<long> test{12321532,6533222662,5645445,2235253255,56346534,3456376,653636,346654,234,2};
+//    for(auto &i:test){
+//        auto use = std::bind(check_prime,i);
+//        pool.add_task(use);
+//    }
+    for (int i = 0; i < 100; ++i) {
+        pool.add_task([]{++num;});
+    }
+    time
+
+    cout<<num<<endl;
+
+    return 0;
+}
---
+++ threadpool.h
@@ -0,0 +1,74 @@
+//
+// Created by meshate on 2019/11/26.
+//
+
+#ifndef NOGU_THREADPOOL_H
+#define NOGU_THREADPOOL_H
+
+#include <future>
+#include <functional>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <condition_variable>
+
+namespace nogu {
+    class threadpool {
+    public:
+        threadpool(size_t _n);
+        ~threadpool();
+
+        template<typename F>
+        void add_task(F &&f);
+
+    private:
+        std::queue<std::function<void()>> tasks_;
+        std::vector<std::thread> workers_;
+        std::mutex mtx_;
+        std::condition_variable cv_;
+        bool shutdown = false;
+    };
+
+    threadpool::threadpool(size_t _n) {
+        for (int i = 0; i < _n; ++i) {
+            workers_.emplace_back([this]() {
+                std::unique_lock<std::mutex> lk(mtx_);
+                for (;;) {
+                    if (!tasks_.empty()) {
+                        auto task = std::move(tasks_.front());
+                        tasks_.pop();
+                        lk.unlock();
+                        task();
+                        lk.lock();
+                    } else if (shutdown) {
+                        break;
+                    } else {
+                        cv_.wait(lk);
+                    }
+                }
+            });
+        }
+    }
+
+    threadpool::~threadpool() {
+        {
+            std::unique_lock<std::mutex> lk(mtx_);
+            shutdown = true;
+        }
+        cv_.notify_all();
+        for(auto &i:workers_){
+            i.join();
+        }
+    }
+
+    template<typename F>
+    void threadpool::add_task(F &&f) {
+        {
+            std::unique_lock<std::mutex> lk(mtx_);
+            tasks_.emplace(std::forward<F>(f));
+        }
+        cv_.notify_one();
+    }
+}
+
+#endif //NOGU_THREADPOOL_H
